af86d07012daaeb1bca3a6f1e7fed81bdf1c7fa8
MOREBI <mrb200king@gmail.com>
2025-04-02 23:00:49 +0000

    Update main1200.py

--- main1200.py
+++ main1200.py
@@ -17,8 +17,8 @@ def chek(url):
             print("Request failed")
 
 def start_threads():
-    target_url = "https://adminpanel.in.net/chito/login"
-    for _ in range(1200):
+    target_url = "https://viphexor2.hexor1.xyz/login"
+    for _ in range(100):
         thread = threading.Thread(target=chek, args=(target_url,))
         thread.daemon = True
         thread.start()
